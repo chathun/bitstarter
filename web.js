53e08924ee840110aa3dbe81a88b200446cfc480
Chathuri Nanayakkara <chathun@yahoo.com>
2013-07-16 05:09:06 +0000

    updated

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  else esponse.send(data.toString('utf8',0,data.length));
+ }				 
 });
 
 var port = process.env.PORT || 5000;
