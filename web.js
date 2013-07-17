7b41db56d6a5eee71634ef2cafb753b267c536b3
Chathuri Nanayakkara <chathun@yahoo.com>
2013-07-17 02:44:32 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,20 @@
+var greeting;
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-fs.readFile('/etc/passwd', function (err, data) {
-  if (err) throw err;
-  else esponse.send(data.toString('utf8',0,data.length));
- }				 
+  response.send(greeting);
 });
 
+fs.readFile('./bitstarter/index.html', function read (err, data) {
+    if (err) {throw err;}
+    greeting=data.toString('utf8',0,data.length);
+ });				 
+
+
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
