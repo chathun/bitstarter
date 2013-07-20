1878ed668d475c84d472e10a4fa566fa8b461e4a
Chathuri Nanayakkara <chathun@yahoo.com>
2013-07-20 14:52:21 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -20,16 +20,16 @@ References:
 var fs=require('fs');
 var program =require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT='index.html';
+var HTMLFILE_DEFAULT="index.html";
 var CHECKSFILE_DEFAULT="checks.json";
 
-var asserFileExists=function(infile){
+var assertFileExists=function(infile){
     var instr=infile.toString();
     if(!fs.existsSync(instr)){
 	console.log("%s does not exist.  Exiting.",instr);
 	process.exit(1);//http://nodejs.org/api/process.html#process_process_exit_code
 	}
-    retrun instr;
+    return instr;
     };
 
 var cheerioHtmlFile=function(htmlfile){
@@ -59,7 +59,7 @@ var clone=function(fn){
 
 if(require.main==module){
     program
-    .option('-c,--checks<check_file>','Path to checks.json',clone(assetFileExists),CHECKSFILE_DEFAULT)
+    .option('-c,--checks<check_file>','Path to checks.json',clone(assertFileExists),CHECKSFILE_DEFAULT)
     .option('-f,--file<html_file>','Path to checks.json',clone(assertFileExists),HTMLFILE_DEFAULT)
 	.parse(process.argv);
     var checkJson=checkHtmlFile(program.file,program.checks);
